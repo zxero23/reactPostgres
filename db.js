c1c8ae287fcc98a1ca11585821ca294ac8890c03
FacundoG <zxero22@gmail.com>
2020-11-27 18:06:10 -0300

    inicio repo

---
+++ db.js
@@ -0,0 +1,12 @@
+const Pool=require("pg").Pool;
+
+const pool = new Pool({
+    user:"postgres",
+    password:"admin",
+    host:"localhost",
+    port:"5432",
+    database:"safiDB",
+
+});
+
+module.exports=pool;
